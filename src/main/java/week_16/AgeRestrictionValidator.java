8382aff440a178f67b67e3a9df78e94d33fdf3cc
Gurol GOKYAR <gurolgokyar@gmail.com>
2023-12-23 00:00:41 +0300

    Added new test cases

--- src/main/java/week_16/AgeRestrictionValidator.java
+++ src/main/java/week_16/AgeRestrictionValidator.java
@@ -1,2 +1,10 @@
-package main.java.week_16;public class AgeRestrigtionValidator {
+package main.java.week_16;
+
+public class AgeRestrictionValidator {
+
+    private static final int MIN_AGE = 18;
+    private static final int MAX_AGE = 65;
+    public boolean isEligible(int age) {
+        return age >= MIN_AGE && age <= MAX_AGE;
+    }
 }
--- src/test/java/week_16/AgeRestrictionValidatorTest.java
+++ src/test/java/week_16/AgeRestrictionValidatorTest.java
@@ -1,2 +1,68 @@
-package test.java.week_16;public class AgeRestrictionValidatorTest {
+package test.java.week_16;
+
+import main.java.week_16.AgeRestrictionValidator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AgeRestrictionValidatorTest {
+
+    AgeRestrictionValidator ageRestrictionValidator = new AgeRestrictionValidator();
+
+    @Test
+    void testIsEligibleWithAgeJustBelowMinimum(){
+        boolean result = ageRestrictionValidator.isEligible(17);
+        assertFalse(result);
+    }
+
+    @Test
+    void testIsEligibleWithAgeAtMinimum(){
+        boolean result = ageRestrictionValidator.isEligible(18);
+        assertTrue(result);
+    }
+
+    @Test
+    void testIsEligibleWithAgeJustAboveMinimumValue(){
+        boolean result = ageRestrictionValidator.isEligible(19);
+        assertTrue(result);
+    }
+
+    @Test
+    void testIsEligibleWithValidValue(){
+        boolean result = ageRestrictionValidator.isEligible(64);
+        assertTrue(result);
+    }
+
+    @Test
+    void testIsEligibleWithAgeJustBelowMaximum(){
+        boolean result = ageRestrictionValidator.isEligible(64);
+        assertTrue(result);
+    }
+
+    @Test
+    void testIsEligibleWithAgeAtMaximumValue(){
+        boolean result = ageRestrictionValidator.isEligible(65);
+        assertTrue(result);
+    }
+
+    @Test
+    void testIsEligibleWithAgeJustAboveMaximum(){
+        boolean result = ageRestrictionValidator.isEligible(66);
+        assertFalse(result);
+    }
+
+    @Test
+    void testIsEligibleWithExtremeLowAge(){
+        boolean result = ageRestrictionValidator.isEligible(0);
+        assertFalse(result);
+    }
+
+    @Test
+    void testIsEligibleWithExtremeHighAge(){
+        boolean result = ageRestrictionValidator.isEligible(64);
+        assertTrue(result);
+    }
+
+
 }
